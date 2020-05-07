1ec9ef222fbcf18d1fe779740bbb633ff3da2ecf
sampada8217 <sampadatirthkar@gmail.com>
2020-05-07 16:56:20 +0530

    Added method printMax to print the max value to std output

--- src/main/java/com/TestMaximumUsingGenerics/Maximum.java
+++ src/main/java/com/TestMaximumUsingGenerics/Maximum.java
@@ -1,15 +1,14 @@
 package com.TestMaximumUsingGenerics;
 
 public class Maximum<E extends  Comparable<E>> {
- E[] elements;
+    E[] elements;
+    @SafeVarargs
     public Maximum(E... elements){
         this.elements=elements;
 
     }
-    public Maximum() {
 
-    }
-
+    @SafeVarargs
     public static <E extends  Comparable<E>> E testMaxValue(E ... elements) {
         E max = elements[0];
         for (E element : elements) {
@@ -18,6 +17,15 @@ public class Maximum<E extends  Comparable<E>> {
             }
         }
         return max;
+    }
+
+    public static <E extends Comparable<E> > void printMax(){
+        System.out.println("Max Integer="+testMaxValue(4,5,6,3,2));
+        System.out.println("Max Float="+testMaxValue(4.5F,5.5F,6.5F,3.5F,2.5F));
+        System.out.println("Max String="+testMaxValue("Apple","Banana","Peach","Cherries"));
+    }
+    public static void main(String[] args) {
+        printMax();
     }
 
 }
--- src/test/java/com/TestMaximumUsingGenerics/MaximumTest.java
+++ src/test/java/com/TestMaximumUsingGenerics/MaximumTest.java
@@ -7,85 +7,73 @@ public class MaximumTest {
 
     @Test
     public void givenMaxValue_atPos1_shouldReturn_sameNumber() {
-        Maximum maximum = new Maximum();
-        int max = (int)maximum.testMaxValue(6, 4, 5,3,2);
+        int max = Maximum.testMaxValue(6, 4, 5,3,2);
         Assert.assertEquals(6, max);
     }
 
     @Test
     public void givenMaxValue_atPos2_shouldReturn_sameNumber() {
-        Maximum maximum = new Maximum();
-        int max = (int)maximum.testMaxValue(4, 6, 5,3,2);
+        int max = Maximum.testMaxValue(4, 6, 5,3,2);
         Assert.assertEquals(6, max);
     }
 
 
     @Test
     public void givenMaxValue_atPos3_shouldReturn_sameNumber() {
-        Maximum maximum = new Maximum();
-        Comparable max=maximum.testMaxValue(4,5,6,3,2);
+        Comparable max= Maximum.testMaxValue(4,5,6,3,2);
         Assert.assertEquals(6, max);
     }
     @Test
     public void givenMaxValue_atPos4_shouldReturn_sameNumber() {
-        Maximum maximum = new Maximum();
-        int max = (int)maximum.testMaxValue(4, 5,3,6,2);
+        int max = Maximum.testMaxValue(4, 5,3,6,2);
         Assert.assertEquals(6, max);
     }
 
     @Test
     public void givenMaxValueFloat_atPos1_shouldReturn_sameNumber() {
-        Maximum maximum = new Maximum();
-        Comparable max = maximum.testMaxValue(6.5F, 4.5F, 5.5F,3.5F,2.5F);
+        Comparable max = Maximum.testMaxValue(6.5F, 4.5F, 5.5F,3.5F,2.5F);
         Assert.assertEquals(6.5F,max);
     }
 
     @Test
     public void givenMaxValueFloat_atPos2_shouldReturn_sameNumber() {
-        Maximum maximum = new Maximum();
-        Comparable max = maximum.testMaxValue(4.5F, 6.5F, 5.5F,3.5F,2.5F);
+        Comparable max = Maximum.testMaxValue(4.5F, 6.5F, 5.5F,3.5F,2.5F);
         Assert.assertEquals(6.5F, max);
 
     }
 
     @Test
     public void givenMaxValueFloat_atPos3_shouldReturn_sameNumber() {
-        Maximum maximum = new Maximum();
-        Comparable max = maximum.testMaxValue(4.5F, 5.5F, 6.5F,3.5F,2.5F);
+        Comparable max = Maximum.testMaxValue(4.5F, 5.5F, 6.5F,3.5F,2.5F);
         Assert.assertEquals(6.5F,max);
 
     }
     @Test
     public void givenMaxValueFloat_atPos4_shouldReturn_sameNumber() {
-        Maximum maximum = new Maximum();
-        Comparable max = maximum.testMaxValue(4.5F, 5.5F, 3.5F,6.5F,2.5F);
+        Comparable max = Maximum.testMaxValue(4.5F, 5.5F, 3.5F,6.5F,2.5F);
         Assert.assertEquals(6.5F,max);
 
     }
 
     @Test
     public void givenMaxString_atPos1_shouldReturn_sameString() {
-        Maximum maximum=new Maximum();
-       String max=(String) maximum.testMaxValue("Banana","Apple","Peach","Cherries");
+        String max= Maximum.testMaxValue("Banana","Apple","Peach","Cherries");
         Assert.assertEquals("Peach",max);
     }
     @Test
     public void givenMaxString_atPos2_shouldReturn_sameString() {
-        Maximum maximum=new Maximum();
-        String max=(String) maximum.testMaxValue("Apple","Banana","Peach","Cherries");
+        String max= Maximum.testMaxValue("Apple","Banana","Peach","Cherries");
         Assert.assertEquals("Peach",max);
     }
     @Test
     public void givenMaxString_atPos3_shouldReturn_sameString() {
-        Maximum maximum=new Maximum();
-        String max= (String) maximum.testMaxValue("Apple","Peach","Banana","Cherries");
+        String max= Maximum.testMaxValue("Apple","Peach","Banana","Cherries");
         Assert.assertEquals("Peach",max);
     }
 
     @Test
     public void givenMaxString_atPos4_shouldReturn_sameString() {
-        Maximum maximum=new Maximum();
-        String max= (String) maximum.testMaxValue("Apple","Peach","Cherries","Banana");
+        String max= Maximum.testMaxValue("Apple","Peach","Cherries","Banana");
         Assert.assertEquals("Peach",max);
     }
 
