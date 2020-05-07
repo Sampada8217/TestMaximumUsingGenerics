4d99f628a2d72a168689f95776063d26d7469adc
sampada8217 <sampadatirthkar@gmail.com>
2020-05-07 15:13:13 +0530

    Added Code of find Maximum Number between three Float Number

--- src/main/java/com/TestMaximumUsingGenerics/Maximum.java
+++ src/main/java/com/TestMaximumUsingGenerics/Maximum.java
@@ -2,22 +2,36 @@ package com.TestMaximumUsingGenerics;
 
 public class Maximum {
 
-    public Integer testMaxValue(Integer x ,Integer y,Integer z) {
-         Integer maxNumber=z;
+    public Integer testMaxValue(Integer x, Integer y, Integer z) {
+        Integer maxNumber = z;
 
-         if(y.compareTo(maxNumber) > 0){
-             maxNumber=y;
+        if (y.compareTo(maxNumber) > 0) {
+            maxNumber = y;
 
-         }
-         else if (x.compareTo(maxNumber)>0){
-             maxNumber=x;
-         }
-         else
-             maxNumber=z;
+        } else if (x.compareTo(maxNumber) > 0) {
+            maxNumber = x;
+        } else
+            maxNumber = z;
 
-         return maxNumber;
+        return maxNumber;
+    }
+
+
+    public static Float testMaxFloatValue(Float a, Float b, Float c) {
+        Float maxNumberFloat = c;
+
+        if (b.compareTo(maxNumberFloat) > 0) {
+            maxNumberFloat = b;
+
+        } else if (a.compareTo(maxNumberFloat) > 0) {
+            maxNumberFloat = a;
+        } else
+            maxNumberFloat = c;
+
+        return maxNumberFloat;
     }
 }
+
 
 
 
--- src/test/java/com/TestMaximumUsingGenerics/MaximumTest.java
+++ src/test/java/com/TestMaximumUsingGenerics/MaximumTest.java
@@ -4,27 +4,63 @@ import org.junit.Assert;
 import org.junit.Test;
 
 
-    public class MaximumTest {
-        @Test
-        public void givenMaxValue_atPos1_shouldReturn_sameNumber() {
-            Maximum maximum =new Maximum();
-            int maxNumber=maximum.testMaxValue(6,4,5);
-            Assert.assertEquals(6,maxNumber);
-        }
 
-        @Test
-        public void givenMaxValue_atPos2_shouldReturn_sameNumber() {
-            Maximum maximum=new Maximum();
-            int maxNumber=maximum.testMaxValue(4,6,5);
-            Assert.assertEquals(6,maxNumber);
-        }
+public class MaximumTest {
 
-        @Test
-        public void givenMaxValue_atPos3_shouldReturn_sameNumber() {
-            Maximum maximum=new Maximum();
-            int maxNumber=maximum.testMaxValue(4,5,6);
-            Assert.assertEquals(6,maxNumber);
-        }
+
+    @Test
+    public void givenMaxValue_atPos1_shouldReturn_sameNumber() {
+        Maximum maximum = new Maximum();
+        int max = maximum.testMaxValue(6, 4, 5);
+        Assert.assertEquals(6, max);
     }
+
+    @Test
+    public void givenMaxValue_atPos2_shouldReturn_sameNumber() {
+        Maximum maximum = new Maximum();
+        int max = maximum.testMaxValue(4, 6, 5);
+        Assert.assertEquals(6, max);
+    }
+
+
+    @Test
+    public void givenMaxValue_atPos3_shouldReturn_sameNumber() {
+        Maximum maximum = new Maximum();
+        int max = maximum.testMaxValue(4,5,6);
+        Assert.assertEquals(6, max);
+    }
+
+
+
+
+    @Test
+    public void givenMaxValueFloat_atPos1_shouldReturn_sameNumber() {
+        Maximum maximum = new Maximum();
+        Comparable maxNumberFloat = maximum.testMaxFloatValue(6.5F, 4.5F, 5.5F);
+        Assert.assertEquals(6.5F,maxNumberFloat);
+    }
+
+    @Test
+    public void givenMaxValueFloat_atPos2_shouldReturn_sameNumber() {
+        Maximum maximum = new Maximum();
+        Comparable maxNumberFloat = maximum.testMaxFloatValue(4.5F, 6.5F, 5.5F);
+        Assert.assertEquals(6.5F, maxNumberFloat);
+
+    }
+
+    @Test
+    public void givenMaxValueFloat_atPos3_shouldReturn_sameNumber() {
+        Maximum maximum = new Maximum();
+        Comparable maxNumberFloat = maximum.testMaxFloatValue(4.5F, 5.5F, 6.5F);
+        Assert.assertEquals(6.5F,maxNumberFloat);
+
+    }
+
+
+}
+
+
+
+
 
 
